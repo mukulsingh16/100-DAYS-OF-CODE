569d8a71bfeb260d3df99793d128ca7f8c2c41d1
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-01-26 21:14:32 +0530

    Create REVERSE.java

---
+++ DAY 13/REVERSE.java
@@ -0,0 +1,15 @@
+class Reverse
+{
+	public static void main (String args[])
+	{
+	  int num=987654321;
+	  while(num!=0)
+	  {
+	    
+	    System.out.print(num%10);
+	    num=num/10;
+	  }
+
+	    
+	}
+}
