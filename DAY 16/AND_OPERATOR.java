03fd707126858512a3863428367fac5cd0a4790c
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-01-26 21:25:45 +0530

    Create AND_OPERATOR.java

---
+++ DAY 16/AND_OPERATOR.java
@@ -0,0 +1,28 @@
+class Exercise1
+{
+	public static void main(String args[])
+	{
+	    for(int i=1;i<=50;i++)
+	    {
+	       if(i%3==0 && i%5==0)
+	       {
+	         System.out.println("heha");
+	       }
+
+	        else if(i%5==0)
+	       {
+	         System.out.println("haha");
+	       }
+
+	        else if (i%3==0)
+	       {
+	         System.out.println("hehe");
+	       }
+
+	       else
+	       {
+	         System.out.println(i);
+	       }
+	    }
+	}
+}
