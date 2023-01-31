af5a4f460fe4408a828d8685358f375f293093f2
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-01-31 19:19:33 +0530

    Create USERARRAY.java

---
+++ DAY 26/USERARRAY.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+class Test
+{
+	void Array()
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int arr[]=new int[5];
+	    System.out.println("enter your array element");
+	    for(int i=0;i<arr.length;i++)
+	    {
+           arr[i]=sc.nextInt();
+	    }
+        System.out.println("your array is:");
+	    for(int i=0;i<arr.length;i++)
+	    {
+	       System.out.print(arr[i]+"at"+(i+1));
+	    }
+	}
+}
+
+class Userarray
+{
+	public static void main(String args[])
+	{
+       Test t=new Test();
+       t.Array();
+	   // Scanner sc=new Scanner(System.in);
+	   //  int arr[]=new int[5];
+	   //  System.out.println("enter your array element");
+	   //  for(int i=0;i<arr.length;i++)
+	   //  {
+       //     arr[i]=sc.nextInt();
+	   //  }
+       //  System.out.println("your array is:");
+	   //  for(int i=0;i<arr.length;i++)
+	   //  {
+	   //     System.out.print(arr[i]+" ");
+	   //  }
+
+	}
+}
