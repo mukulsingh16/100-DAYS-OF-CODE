c5c9a77d6d0779ad29955b897127f44777ab07f0
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-02-04 22:17:52 +0530

    Create PACKAGE_EX1.java

---
+++ DAY 39/PACKAGE_EX1.java
@@ -0,0 +1,17 @@
+package mukul;
+public class packageEx1
+{
+	// public static void main(String[] args) {
+	
+	// System.out.println("my name is mukul");
+
+	public void fun()
+	{
+	   System.out.println("my name is gagan");
+	}
+	public void fun2()
+	{
+	   System.out.println("my name is mukul gagan");
+	}
+// }
+}
