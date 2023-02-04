21bbebb8f4ca2504b61614d64aa6e377e1feae24
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-02-04 22:18:35 +0530

    Create PACKAGE_EX2.java

---
+++ DAY 40/PACKAGE_EX2.java
@@ -0,0 +1,12 @@
+package mukul2;
+import mukul.packageEx1;
+class packageEx2 extends packageEx1
+{
+public static void main(String agrs[])
+{
+   System.out.println("my name is harry");
+   packageEx2 p1=new packageEx2();
+   p1.fun();
+   p1.fun2();
+}
+}
