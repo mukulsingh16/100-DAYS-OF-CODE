3a6674b52d38a4ca3bb3ec47b845b86b475b2729
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-02-13 23:34:36 +0530

    Update USERARRAY.java

--- DAY 26/USERARRAY.java
+++ DAY 26/USERARRAY.java
@@ -24,18 +24,6 @@ class Userarray
 	{
        Test t=new Test();
        t.Array();
-	   // Scanner sc=new Scanner(System.in);
-	   //  int arr[]=new int[5];
-	   //  System.out.println("enter your array element");
-	   //  for(int i=0;i<arr.length;i++)
-	   //  {
-       //     arr[i]=sc.nextInt();
-	   //  }
-       //  System.out.println("your array is:");
-	   //  for(int i=0;i<arr.length;i++)
-	   //  {
-	   //     System.out.print(arr[i]+" ");
-	   //  }
 
 	}
 }
