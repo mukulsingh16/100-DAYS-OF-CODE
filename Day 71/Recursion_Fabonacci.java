31aeffa9bfff1f7408203711a9aac6e814fef2fb
mukulsingh16 <64323837+mukulsingh16@users.noreply.github.com>
2023-04-23 19:54:03 +0530

    Create Recursion_Fabonacci.java

---
+++ Day 71/Recursion_Fabonacci.java
@@ -0,0 +1,20 @@
+public class recursionfacbonacci {
+    public static void PrintFab(int a, int b,int n)
+    {
+        if(n==0)
+        {
+            return;
+        }
+        int c=a+b;
+        System.out.println(c);
+        PrintFab(b, c, n-1);
+
+    }
+    public static void main(String[] args) {
+        int a=0,b=1;
+        System.out.println(a);
+        System.out.println(b);
+        int n=7;
+        PrintFab(a, b, n-2);
+    }
+}
